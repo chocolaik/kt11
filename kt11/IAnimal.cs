dd5b819d6786230853ddc7f51c2c01e23bd0ec47
Alexey <chocoladik228@gmail.com>
2026-09-24 13:40:46 +0300

    created IAnimal

---
+++ kt11/IAnimal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kt11
+{
+    public interface IAnimal
+    {
+        string Name { get; }
+        void MakeSound();
+    }
+}
--- kt11/Program.cs
+++ kt11/Program.cs
@@ -2,82 +2,79 @@
 using System.Collections.Generic;
 using System.Text;
 
-public interface IAnimal
-{
-    string Name { get; }
-    void MakeSound();
-}
 
-public interface ITrainble : IAnimal
-{
-    string Train();
-}
-public class Lion : IAnimal
-{
-    public string Name { get; private set; }
-    public Lion(string name)
-    {  Name = name; }
-    public void MakeSound()
+namespace kt11 {
+    public interface ITrainble : IAnimal
     {
-        Console.WriteLine("Ррррррр");
+        string Train();
     }
-}
-
-public class Dog : ITrainble
-{
-    public string Name { get; private set; }
-    public Dog(string name)
+    public class Lion : IAnimal
     {
-        Name = name;
-    }
-    public void MakeSound()
-    {
-        Console.WriteLine("Гав");
-    }
-    public string Train()
-    {
-        return $"{Name}: Бегает за костью";
-    }
-}
-
-public class Dolphin : ITrainble
-{
-    public string Name { get; private set; }
-    public Dolphin(string name)
-    {
-        Name = name;
-    }
-    public void MakeSound()
-    {
-        Console.WriteLine("Свист");
-    }
-    public string Train()
-    {
-        return $"{Name}: Держит мяч";
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.OutputEncoding = Encoding.UTF8;
-
-        List<IAnimal> animals = new List<IAnimal>
+        public string Name { get; private set; }
+        public Lion(string name)
+        {  Name = name; }
+        public void MakeSound()
         {
-            new Lion("Лев"),
-            new Dog("Собака"),
-            new Dolphin("Дельфин")
-        };
-        foreach (var animal in animals)
+            Console.WriteLine("Ррррррр");
+        }
+    }
+
+    public class Dog : ITrainble
+    {
+        public string Name { get; private set; }
+        public Dog(string name)
         {
-            Console.WriteLine($"Животное: {animal.Name}");
-            animal.MakeSound();
-            if (animal is ITrainble trainableAnimal)
+            Name = name;
+        }
+        public void MakeSound()
+        {
+            Console.WriteLine("Гав");
+        }
+        public string Train()
+        {
+            return $"{Name}: Бегает за костью";
+        }
+    }
+
+    public class Dolphin : ITrainble
+    {
+        public string Name { get; private set; }
+        public Dolphin(string name)
+        {
+            Name = name;
+        }
+        public void MakeSound()
+        {
+            Console.WriteLine("Свист");
+        }
+        public string Train()
+        {
+            return $"{Name}: Держит мяч";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            List<IAnimal> animals = new List<IAnimal>
             {
-                Console.WriteLine(trainableAnimal.Train());
+                new Lion("Лев"),
+                new Dog("Собака"),
+                new Dolphin("Дельфин")
+            };
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"Животное: {animal.Name}");
+                animal.MakeSound();
+                if (animal is ITrainble trainableAnimal)
+                {
+                    Console.WriteLine(trainableAnimal.Train());
+                }
+                Console.WriteLine();
             }
-            Console.WriteLine();
         }
     }
 }
